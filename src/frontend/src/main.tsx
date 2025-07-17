e8543ec53e9647ba3681307f1e310b12c8211fdd
yylunxie <yylunxie@gmail.com>
2025-07-17 15:40:02 +0800

    chore: wrap application in React.StrictMode for improved development checks

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,7 +6,9 @@ import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
